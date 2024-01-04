727124f7b0fbcf447578b4f9d014611f3510dfa6
Aimen Elahi <aimenqaiser2000@gmail.com>
2024-01-04 14:25:01 -0800

    fixing cards

--- src/pages/SalesRep.jsx
+++ src/pages/SalesRep.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TableComponent from "../components/TableComponent";
+import { UsersRound, AlignStartHorizontal, MonitorCheck } from "lucide-react";
 
 const SALESREP_HEADERS = ["ID", "Name", "Territory", "Joining Date", "Status"];
 
@@ -24,27 +25,42 @@ const SalesRep = () => {
       {/* Display Stats Cards */}
       <div className="flex gap-4">
         {/* Card 1 */}
-        <div className="flex-1 p-4 border rounded-lg ">
-          <h2 className="text-lg font-semibold mb-2 text-gray-500">
-            Total Sales Reps
-          </h2>
-          <p className="text-3xl font-bold">10</p>
+        <div className="flex-1 p-4 border rounded-lg flex items-center">
+          <div className="w-14 h-14 flex items-center justify-center bg-gray-800 rounded-full">
+            <UsersRound size={24} className="text-white" />
+          </div>
+          <div className="ml-3">
+            <h2 className="text-sm font-semibold mb-1 text-gray-600">
+              Total Sales Reps
+            </h2>
+            <p className="text-xl font-bold text-black">10</p>
+          </div>
         </div>
 
         {/* Card 2 */}
-        <div className="flex-1 p-4 border rounded-lg ">
-          <h2 className="text-lg font-semibold mb-2 text-gray-500">
-            Active Sales Reps
-          </h2>
-          <p className="text-3xl font-bold">8</p>
+        <div className="flex-1 p-4 border rounded-lg flex items-center">
+          <div className="w-14 h-14 flex items-center justify-center bg-gray-800 rounded-full">
+            <MonitorCheck size={24} className="text-white" />
+          </div>
+          <div className="ml-3">
+            <h2 className="text-sm font-semibold mb-1 text-gray-600">
+              Active Sales Reps
+            </h2>
+            <p className="text-xl font-bold text-black">8</p>
+          </div>
         </div>
 
         {/* Card 3 */}
-        <div className="flex-1 p-4 border rounded-lg ">
-          <h2 className="text-lg font-semibold mb-2 text-gray-500">
-            Top Performing Territory
-          </h2>
-          <p className="text-3xl font-bold">Region A</p>
+        <div className="flex-1 p-4 py-8 border rounded-lg flex items-center">
+          <div className="w-14 h-14 flex items-center justify-center bg-gray-800 rounded-full">
+            <AlignStartHorizontal size={24} className="text-white" />
+          </div>
+          <div className="ml-3">
+            <h2 className="text-sm font-semibold mb-1 text-gray-600">
+              Top Performing Territory
+            </h2>
+            <p className="text-xl font-bold text-black">Region A</p>
+          </div>
         </div>
       </div>
 
